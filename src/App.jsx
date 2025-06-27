775208af99064e9b6a055725bdfabec2658cd6e8
Abhirami B <137210545+abhirami102@users.noreply.github.com>
2025-06-27 23:10:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import VehicleDetails from './component/Vehicle_details'
 import Background from './component/Background'
+import Dashboard from './component/DashboardHome'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Background />
       <div className="flex items-center justify-center absolute inset-0 z-2">
       <VehicleDetails />
+      <Dashboard />
       </div>
       </>
   )
